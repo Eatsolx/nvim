a11b26b47ecd6e922df6263bef567d32ae309140
Eatsolx <eatsol.x@gmail.com>
2025-06-17 23:34:10 +0800

    Add markdown-preview && Delete peek.nvim

--- lua/plugins/peek.lua
+++
@@ -1,9 +0,0 @@
--- markdown预览
-return {
-  "toppair/peek.nvim",
-  event = "VeryLazy",
-  build = "deno task --quiet build:fast",
-  config = function()
-      require "plugins.config.peek"
-  end,
-}
