900627715325cd829b9003b1a35f1686f7e92c03
Eatsolx <eatsol.x@gmail.com>
2025-07-08 16:25:01 +0800

    update

--- lua/custom/keybindings.lua
+++ lua/custom/keybindings.lua
@@ -38,11 +38,11 @@ map("n", "<leader>h", ":nohlsearch<CR>", opts)
 -- map({"n", "v"}, "<leader>p", [["+p]], opts)
 
 -- code_runner
-vim.keymap.set('n', '<leader>rr', ':RunCode<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
+map('n', '<leader>rr', ':RunCode<CR>', { noremap = true, silent = false })
+map('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
+map('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
+map('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
+map('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
+map('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
+map('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
 
