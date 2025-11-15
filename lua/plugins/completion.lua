117ca39056ddcac0526b18c44666391a8785b90c
Eatsolx <eatsol.x@gmail.com>
2025-11-15 23:21:17 +0800

    modify keymaps for blink.cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -11,6 +11,24 @@ return {
 		keymap = {
 			["<C-u>"] = { "scroll_documentation_up", "fallback" },
 			["<C-d>"] = { "scroll_documentation_down", "fallback" },
+			["<Tab>"] = {
+				"snippet_forward",
+				"select_next",
+				function(cmp)
+				  if has_words_before() or vim.api.nvim_get_mode().mode == "c" then return cmp.show() end
+				end,
+				"fallback",
+			},
+			["<S-Tab>"] = {
+			    "select_prev",
+			    "snippet_backward",
+			    function(cmp)
+					if vim.api.nvim_get_mode().mode == "c" then return cmp.show() end
+			    end,
+			    "fallback",
+			},
+			-- ["<CR>"] = { "accept", "fallback" },
+			["<C-e>"] = { "hide", "fallback" },
 		},
 		signature = {
 			enabled = true,
