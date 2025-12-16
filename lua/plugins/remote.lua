fea2825e244f31f64c6f46cad56c1037cbde67c0
Eatsolx <eatsol.x@gmail.com>
2025-12-16 23:45:53 +0800

    edit remote.lua

--- lua/plugins/remote.lua
+++ lua/plugins/remote.lua
@@ -1,8 +1,13 @@
 return {
-    'nosduco/remote-sshfs.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-    opts = {
-        -- Refer to the configuration section below
-        -- or leave empty for defaults
+    "amitds1997/remote-nvim.nvim",
+    version = "*", -- Pin to GitHub releases
+    dependencies = {
+        "nvim-lua/plenary.nvim", -- For standard functions
+        "MunifTanjim/nui.nvim", -- To build the plugin UI
+        "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
     },
+    -- config = true,
+	config = function ()
+		require "plugins.config.remote"
+	end,
 }
